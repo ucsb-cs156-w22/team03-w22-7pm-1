2dfd7564aecadd0479666e333e59b92ddf88ce59
WAKACHUU <yijiecai@ucsb.edu>
2022-02-26 17:57:10 -0800

    kc - appnavbar mutation test fixed

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -113,10 +113,10 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
               {
                 hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="CollegiateSubreddits" id="appnavbar-collegiatesubreddits-dropdown" data-testid="appnavbar-collegiatesubreddits-dropdown" >
-                    <NavDropdown.Item href="/collegiatesubreddits/list">List</NavDropdown.Item>
+                    <NavDropdown.Item href="/collegiatesubreddits/list" data-testid="appnavbar-collegiatesubreddits-list">List</NavDropdown.Item>
                     {
                       hasRole(currentUser, "ROLE_ADMIN") && (
-                        <NavDropdown.Item href="/collegiatesubreddits/create">Create</NavDropdown.Item>
+                        <NavDropdown.Item href="/collegiatesubreddits/create" data-testid="appnavbar-collegiatesubreddits-create">Create</NavDropdown.Item>
                       )
                     }
                   </NavDropdown>
